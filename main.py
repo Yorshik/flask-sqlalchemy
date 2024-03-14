daa0cce0c901a90d58b5026abd09c6c09c33e266
Yorshik <kirkidzapp@gmail.com>
2024-03-15 00:19:50 +0300

    Первая работа

--- main.py
+++ main.py
@@ -1,14 +1,27 @@
+import datetime
+
 from flask import Flask
 from data import db_session
 from data.users import User
+from data.jobs import Jobs
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
 
 
-def main():
-    db_session.global_init("db/marsiane.db")
-    db_sess = db_session.create_session()
+def add_worker(sess):
+    worker = Jobs()
+    worker.team_leader = 1
+    worker.job = 'deployment of residential modules 1 and 2'
+    worker.work_size = 15
+    worker.collaborators = '2, 3'
+    worker.start_date = datetime.datetime.now()
+    worker.is_finished = False
+    sess.add(worker)
+    sess.commit()
+
+
+def add_captain_and_others(sess):
     captain = User()
     captain.name = 'Ridley'
     captain.surname = 'Scott'
@@ -17,7 +30,7 @@ def main():
     captain.speciality = 'research engineer'
     captain.address = 'module_1'
     captain.email = 'scott_chief@mars.org'
-    db_sess.add(captain)
+    sess.add(captain)
     names = ['Kirill', 'Andrew', 'Vasya']
     surnames = ['Petrov', 'Ivanov', 'Pupkin']
     ages = [19, 20, 22]
@@ -25,7 +38,9 @@ def main():
     specialities = ['pythonist', 'doctor', 'zam captain']
     addresses = ['module_2', 'module_3', 'module_4']
     emails = ['pythonist@mars.org', 'doctor@mars.org', 'zam_captain@mars.org']
-    for name, surname, age, position, speciality, address, email in zip(names, surnames, ages, positions, specialities, addresses, emails):
+    for name, surname, age, position, speciality, address, email in zip(
+            names, surnames, ages, positions, specialities, addresses, emails
+            ):
         user = User()
         user.name = name
         user.surname = surname
@@ -34,8 +49,15 @@ def main():
         user.speciality = speciality
         user.address = address
         user.email = email
-        db_sess.add(user)
-    db_sess.commit()
+        sess.add(user)
+    sess.commit()
+
+
+def main():
+    db_session.global_init("db/marsiane.db")
+    db_sess = db_session.create_session()
+    # add_captain_and_others(db_sess)
+    add_worker(db_sess)
     # app.run()
 
 
